e60bd70a0772576880b826b758d8557750b38c54
Paul J. Durack <me@pauldurack.com>
2023-06-06 15:26:43 -0700

    start input4MIPs reporting

--- input4MIPsReport.py
+++ input4MIPsReport.py
@@ -9,7 +9,6 @@ Created on Tue Jun  6 14:22:28 2023
 # %% imports
 import os
 
-
 # %% set paths
 basePath = "/p/user_pub/work/input4MIPs/"
 mipEra = ["CMIP6", "CMIP6Plus"]
@@ -17,6 +16,16 @@ mipEra = ["CMIP6", "CMIP6Plus"]
 for phase in mipEra:
     path = os.path.join(basePath, phase)
     mips = os.listdir(path)
-    print("".join([phase, ":", str(len(mips)), " MIPs served"]))
+    print("".join([phase, ": ", str(len(mips)), " MIPs served"]))
     size = os.path.getsize(path)
-    print("".join([phase, ":", str(size/1e12), " size (TB)"]))
+    print("".join([phase, ": ", str(size/1e12), " size (TB)"]))
+    # loop through mips and capture insitution_id's
+    instIds = []
+    for mip in mips:
+        path = os.path.join(basePath, phase, mip)
+        mipList = os.listdir(path)
+        for xmip in mipList:
+            if xmip not in instIds:
+                instIds.append(mip)
+    print("".join([phase, ": ", str(len(instIds)), " total instIds"]))
+    print(instIds)      
