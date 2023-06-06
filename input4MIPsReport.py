d1d1dd25c32cd722cba8254e51516baeb340aa13
Paul J. Durack <me@pauldurack.com>
2023-06-06 14:36:34 -0700

    start input4MIPs reporting

--- input4MIPsReport.py
+++ input4MIPsReport.py
@@ -17,6 +17,6 @@ mipEra = ["CMIP6", "CMIP6Plus"]
 for phase in mipEra:
     path = os.path.join(basePath, phase)
     mips = os.listdir(path)
-    print(phase, ":", len(mips), " MIPs served")
+    print("".join([phase, ":", len(mips), " MIPs served"]))
     size = os.path.getsize(path)
-    print(phase, ":", size/1e12, " size (TB)")
+    print("".join([phase, ":", size/1e12, " size (TB)"]))
